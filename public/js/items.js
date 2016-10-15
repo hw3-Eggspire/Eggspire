5031aea197b9b734983f2efb3bb725c48b0016b5
ryanjiang <ryan.qiyu.jiang@gmail.com>
2016-10-15 19:30:32 -0400

    ryan

--- public/js/items.js
+++ public/js/items.js
@@ -7,6 +7,7 @@
   var expires_dates = snapshot.val().username;
 });
  */
+ var userId='RYAN';
 	// User clicked on the add button
 	// If there is any text inside the item field, add that text to the todo list
 	document.getElementById('add').addEventListener('click', function() {
