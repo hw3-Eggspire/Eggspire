fe01eda7805bef37b27bb7eb609a585a7a0f12e4
ryanjiang <ryan.qiyu.jiang@gmail.com>
2016-10-15 19:10:46 -0400

    sd

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
-
-
-var app = firebase.initializeApp({
-    apiKey: 'AIzaSyBy257LddGZE8X3x4a3Onoojd68sjauOc4',
-    authDomain: 'eggspire.firebaseapp.com',
-    databaseURL: 'https://eggspire.firebaseio.com',
-    storageBucket: 'bucket.appspot.com'
-  });
+var config = {
+    apiKey: "AIzaSyBy257LddGZE8X3x4a3Onoojd68sjauOc4",
+    authDomain: "eggspire.firebaseapp.com",
+    databaseURL: "https://eggspire.firebaseio.com",
+    storageBucket: "eggspire.appspot.com",
+    messagingSenderId: "539280365425"
+  };
+  firebase.initializeApp(config);
