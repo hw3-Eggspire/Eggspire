635489704bc26e7a03aca356235d287002c03ca3
atharva <atarw@users.noreply.github.com>
2016-10-15 16:19:43 -0400

    edited index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,9 @@
+var firebase = require("firebase")
+var config = {
+  apiKey: "AIzaSyBy257LddGZE8X3x4a3Onoojd68sjauOc4",
+  authDomain: "eggspire.firebaseapp.com",
+  databaseURL: "https://eggspire.firebaseio.com",
+  storageBucket: "gs://eggspire.appspot.com"
+};
 
-
-var app = firebase.initializeApp({
-    apiKey: 'AIzaSyBy257LddGZE8X3x4a3Onoojd68sjauOc4',
-    authDomain: 'eggspire.firebaseapp.com',
-    databaseURL: 'https://eggspire.firebaseio.com',
-    storageBucket: 'bucket.appspot.com'
-  });
+firebase.initializeApp(config)
