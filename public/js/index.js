0ea62285ab6b3989a5666c51ec343046a74635c6
ryanjiang <ryan.qiyu.jiang@gmail.com>
2016-10-15 19:04:07 -0400

    Revert "try"
    This reverts commit 8cc589f9b00526efbc97277a098e8d014d7eca34.

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,8 @@
 
 
 var app = firebase.initializeApp({
-    apiKey: "AIzaSyBy257LddGZE8X3x4a3Onoojd68sjauOc4",
-    authDomain: "eggspire.firebaseapp.com",
-    databaseURL: "https://eggspire.firebaseio.com",
-    storageBucket: "eggspire.appspot.com",
-    messagingSenderId: "539280365425"
+    apiKey: 'AIzaSyBy257LddGZE8X3x4a3Onoojd68sjauOc4',
+    authDomain: 'eggspire.firebaseapp.com',
+    databaseURL: 'https://eggspire.firebaseio.com',
+    storageBucket: 'bucket.appspot.com'
   });
