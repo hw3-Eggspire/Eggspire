6433379481b269a0dff1cb54ab08a71a6e67349e
atharva <atarw@users.noreply.github.com>
2016-10-15 21:55:37 -0400

    sign in methods are now google and email

--- public/js/app.js
+++ public/js/app.js
@@ -30,7 +30,7 @@ var uiConfig = {
     },*/
     //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    //firebase.auth.EmailAuthProvider.PROVIDER_ID
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
   ],
   // Terms of service url.
   'tosUrl': 'https://www.google.com'
