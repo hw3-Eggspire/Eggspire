a009e1a2b0c22b135c708127ca266acc58098910
atharva <atarw@users.noreply.github.com>
2016-10-15 20:55:32 -0400

    Revert "Revert "😈  😎  💪  ✊  👏  🙌  🏈""
    This reverts commit 64e137e6415b262a297c5f9b2026b5f3b068e0e4.

--- public/js/items.js
+++ public/js/items.js
@@ -60,19 +60,3 @@ function writeUserData(userId, food, date) {
 		writeUserData(userId,text,today.getTime());
 	}
 
-
-/*
-	$('#add').on('click',function()){
-
-
-		$.ajax({
-		type:'POST',
-		url:'/api/in_fridge',
-	 	data: item,
-	 	success:function(newInFridge){
-	 		$++++++++++++++++++
-	 	}
-		})
-	}
-*/
-
